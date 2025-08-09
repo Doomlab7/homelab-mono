205d1c9efdc15fb1fcc8d492d7f1d6c4a3f5396d
pypeaday <pypeaday@pype.dev>
2025-08-08 19:39:39 -0500

    garbo

--- python/home-automation-pipelines/home_automation_pipelines/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Pypeaday <pypeaday@pm.me>
-#
-# SPDX-License-Identifier: MIT
--- python/home-automation-pipelines/home_automation_pipelines/__main__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Pypeaday <pypeaday@pm.me>
-#
-# SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == "__main__":
-    from .cli import home_automation_pipelines
-
-    sys.exit(home_automation_pipelines())
